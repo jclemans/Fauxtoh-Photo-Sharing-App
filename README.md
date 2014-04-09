bcb01b296093a8fee177ba141cb5576d3829761a
Jeremy Clemans & Harland Hauger <student@epicodus.com>
2014-04-09 09:09:37 -0700

    initial project setup

---
+++ README.md
@@ -0,0 +1,9 @@
+== Fauxtoh Photo Sharing App
+
+Goal: Create a app that lets users upload and share photos
+
+* Let users create accounts
+
+* Let users upload photos using Paperclip gem
+
+* Let users tag other users in photos
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fauxtoh::Application.config.secret_key_base = '8732f6a9bd5a0c419c2202f4e72727e1165e5058caa98a12ceff93ec48923242e6edaf7d081c183eb25de609758161784de4307ca29fb52bfe1d3e962b28af54'
